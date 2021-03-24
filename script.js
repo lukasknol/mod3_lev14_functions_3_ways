87c625e98b3e2e87e160e992d39b076b1c95eb4d
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-03-24 14:46:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 // Function declarations
 function totalDeclaration(number1, number2) {
-   return number1 + number2;
+   const number1Squared = number1 * number1;
+   const number2Squared = number2 * number2;
+   const sum = number1Squared + number2Squared;
+   const sumSquared = sum * sum;
+   return sumSquared;
 }
 
-totalDeclaration(5, 6); // 11
+console.log(totalDeclaration(5, 6)); 
 
 
 // Function expressions
 const totalExpression = function (number1, number2) {
-   return number1 + number2;
+   const number1Squared = number1 * number1;
+   const number2Squared = number2 * number2;
+   const sum = number1Squared + number2Squared;
+   const sumSquared = sum * sum;
+   return sumSquared;
 };
 
-totalExpression(5, 6); // 11
+console.log(totalExpression(5, 6)); 
 
 
 // Arrow functions
 const totalArrow = (number1, number2) => {
-   return number1 + number2;
+   const number1Squared = number1 * number1;
+   const number2Squared = number2 * number2;
+   const sum = number1Squared + number2Squared;
+   const sumSquared = sum * sum;
+   return sumSquared;
 };
 
-totalArrow(5, 6); // 11
+console.log(totalArrow(5, 6));
